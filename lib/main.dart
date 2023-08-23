bfbf03290944a41f091d0d1fff2bdf28b85f0bc2
w1y0n0 <wiyono30@gmail.com>
2023-08-23 09:23:13 +0700

    add module & assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_slicing_ui/core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      navigatorKey: Get.navigatorKey,
       theme: ThemeData(
         // This is the theme of your application.
         //
